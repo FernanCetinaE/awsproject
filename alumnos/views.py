2b8f2af1de2d64f38005c11a756ecfe69aba6eb1
Fernan <a17001017@alumnos.uady.mx>
2023-11-15 01:29:07 -0600

    feat: code improvement

--- alumnos/views.py
+++ alumnos/views.py
@@ -9,7 +9,7 @@ import json
 alumnos = []
 @csrf_exempt
 def alumno_detail(request, pk=None, *args, **kwargs):
-    alumno = None
+    alumno = {}
     # try:
     #     if(pk is None):
     #         return JsonResponse({'alumnos': alumnos}, status=200, safe=False)
@@ -25,57 +25,64 @@ def alumno_detail(request, pk=None, *args, **kwargs):
                 return JsonResponse({'error': 'Profesor no encontrado'}, status=404)
             else:
                 return JsonResponse(filtered[0], status=200)
-        return JsonResponse(alumno, status=201)
+        return JsonResponse(alumnos, status=200, safe=False)
 
 
     elif request.method == 'PUT':
         data = json.loads(request.body)
+        
         if not data:
-            return JsonResponse({'error': 'Datos faltantes'}, status=500)
+            return JsonResponse({'error': 'Datos faltantes'}, status=400)
         if 'nombres' in data:
             if not isinstance(data['nombres'], str):
-                return JsonResponse({'error': 'El campo nombres debe ser una cadena de caracteres'}, status=500)
+                return JsonResponse({'error': 'El campo nombres debe ser una cadena de caracteres'}, status=400)
             alumno['nombres'] = data['nombres']
         if 'apellidos' in data:
             if not isinstance(data['apellidos'], str):
-                return JsonResponse({'error': 'El campo apellidos debe ser una cadena de caracteres'}, status=500)
+                return JsonResponse({'error': 'El campo apellidos debe ser una cadena de caracteres'}, status=400)
             alumno['apellidos'] = data['apellidos']
         if 'matricula' in data:
             if not isinstance(data['matricula'], str):
-                return JsonResponse({'error': 'El campo matricula debe ser una cadena de caracteres'}, status=500)
+                return JsonResponse({'error': 'El campo matricula debe ser una cadena de caracteres'}, status=400)
             alumno['matricula'] = data['matricula']
         if 'promedio' in data:
             try:
                 promedio = float(data['promedio'])
             except ValueError:
-                return JsonResponse({'error': 'El campo promedio debe ser un número decimal'}, status=500)
+                return JsonResponse({'error': 'El campo promedio debe ser un número decimal'}, status=400)
             alumno['promedio'] = promedio
-        return JsonResponse(alumno, status=201)
+        for i, obj in enumerate(alumnos):
+            if obj['id'] == data['id']:
+                # Update the object with new data
+                alumnos[i].update(alumno)
+        return JsonResponse(alumno, status=200, safe=False)
 
     elif request.method == 'DELETE':
-        alumno = [alumno for alumno in alumnos if alumno['id'] == pk]
-        if alumno is None or len(alumno) == 0:
+        filtered = [alumno for alumno in alumnos if alumno['id'] == pk]
+        if pk is None:
+            return JsonResponse({'error': 'Alumno no encontrado'}, status=405)
+        if len(filtered) == 0:
             return JsonResponse({'error': 'Alumno no encontrado'}, status=404)
         
-        alumnos.remove(alumno[0])
+        alumnos.remove(filtered[0])
         return JsonResponse({'message': 'Alumno eliminado correctamente'}, status=200)
 
     elif request.method == 'POST':
         data = json.loads(request.body)
         if not data:
-            return JsonResponse({'error': 'Datos faltantes'}, status=500)
+            return JsonResponse({'error': 'Datos faltantes'}, status=400)
         if 'nombres' not in data or 'apellidos' not in data or 'matricula' not in data or 'promedio' not in data:
-            return JsonResponse({'error': 'Datos incompletos'}, status=500)
+            return JsonResponse({'error': 'Datos incompletos'}, status=400)
         if not isinstance(data['nombres'], str):
-            return JsonResponse({'error': 'El campo nombres debe ser una cadena de caracteres'}, status=500)
+            return JsonResponse({'error': 'El campo nombres debe ser una cadena de caracteres'}, status=400)
         if not isinstance(data['apellidos'], str) or data['apellidos'] is None:
-            return JsonResponse({'error': 'El campo apellidos debe ser una cadena de caracteres'}, status=500)
+            return JsonResponse({'error': 'El campo apellidos debe ser una cadena de caracteres'}, status=400)
         if not isinstance(data['matricula'], str):
-            return JsonResponse({'error': 'El campo matricula debe ser una cadena de caracteres'}, status=500)
+            return JsonResponse({'error': 'El campo matricula debe ser una cadena de caracteres'}, status=400)
         try:
             promedio = int(data['promedio'])
         except ValueError:
-            return JsonResponse({'error': 'El campo promedio debe ser un número decimal'}, status=500)
+            return JsonResponse({'error': 'El campo promedio debe ser un número decimal'}, status=400)
         alumno = {
             'id': data['id'],
             'nombres': data['nombres'],
--- profesores/views.py
+++ profesores/views.py
@@ -9,12 +9,6 @@ profesores = []
 @csrf_exempt
 def profesor_detail(request, pk=None):
     profesor = {}
-    # try:
-    #     if(pk is None and request.method == 'GET'):
-    #         return JsonResponse(profesores, status=200, safe=False)
-    #     profesor = next(profesor for profesor in profesores if profesor['id'] == pk)
-    # except StopIteration:
-    #     return JsonResponse({'error': 'Profesor no encontrado'}, status=404)
 
     if request.method == 'GET':
         #filtrar por id profesores
@@ -24,14 +18,14 @@ def profesor_detail(request, pk=None):
                 return JsonResponse({'error': 'Profesor no encontrado'}, status=404)
             else:
                 return JsonResponse(filtered[0], status=200)
-        return JsonResponse(profesores, status=201)
+        return JsonResponse(profesores, status=200, safe=False)
 
     elif request.method == 'PUT':
         data = json.loads(request.body)
         if not data:
             return JsonResponse({'error': 'Datos faltantes'}, status=400)
         if 'numeroEmpleado' in data:
-            if not isinstance(data['numeroEmpleado'], str):
+            if not isinstance(data['numeroEmpleado'], int):
                 return JsonResponse({'error': 'El campo numeroEmpleado debe ser una cadena de caracteres'}, status=400)
             profesor['numeroEmpleado'] = data['numeroEmpleado']
         if 'nombres' in data:
@@ -43,20 +37,25 @@ def profesor_detail(request, pk=None):
                 return JsonResponse({'error': 'El campo apellidos debe ser una cadena de caracteres'}, status=400)
             profesor['apellidos'] = data['apellidos']
         if 'horasClase' in data:
-            try:
-                horasClase = int(data['horasClase'])
-            except ValueError:
-                return JsonResponse({'error': 'El campo horasClase debe ser un número entero'}, status=400)
-            profesor['horasClase'] = horasClase
-        return JsonResponse(profesor, status=200)
+            if not isinstance(data['horasClase'], int) or data['horasClase'] is None:
+                return JsonResponse({'error': 'El campo horasClase debe ser entero'}, status=400)
+            profesor['horasClase'] = data['horasClase']
+        for i, obj in enumerate(profesores):
+            if obj['id'] == data['id']:
+                profesores[i].update(profesor)
+        return JsonResponse(profesor, status=200, safe=False)
 
     elif request.method == 'DELETE':
-        profesores.remove(profesor)
-        return JsonResponse({'message': 'Profesor eliminado correctamente'}, status=201)
+        if pk is None:
+            return JsonResponse({'error': 'Profesor no encontrado'}, status=405)
+        filtered = [profesor for profesor in profesores if profesor['id'] == pk]
+        if len(filtered) == 0:
+            return JsonResponse({'error': 'Profesor no encontrado'}, status=404)
+        profesores.remove(filtered[0])
+        return JsonResponse({'message': 'Profesor eliminado correctamente'}, status=200)
 
     elif request.method == 'POST':
         data = json.loads(request.body)
-        print(data)
         if not data:
             return JsonResponse({'error': 'Datos faltantes'}, status=400)
         if 'numeroEmpleado' not in data or 'nombres' not in data or 'apellidos' not in data or 'horasClase' not in data:
@@ -67,16 +66,14 @@ def profesor_detail(request, pk=None):
             return JsonResponse({'error': 'El campo nombres debe ser una cadena de caracteres'}, status=400)
         if not isinstance(data['apellidos'], str) or data['apellidos'] is None:
             return JsonResponse({'error': 'El campo apellidos debe ser una cadena de caracteres'}, status=400)
-        try:
-            horasClase = int(data['horasClase'])
-        except ValueError:
-            return JsonResponse({'error': 'El campo horasClase debe ser un número entero'}, status=400)
+        if not isinstance(data['horasClase'], int) or data['horasClase'] is None:
+            return JsonResponse({'error': 'El campo horasClase debe ser entero'}, status=400)
         profesor = {
-            'id': len(profesores) + 1,
+            'id': data['id'],
             'numeroEmpleado': data['numeroEmpleado'],
             'nombres': data['nombres'],
             'apellidos': data['apellidos'],
-            'horasClase': horasClase
+            'horasClase': data['horasClase']
         }
         profesores.append(profesor)
         return JsonResponse(profesor, status=201)
